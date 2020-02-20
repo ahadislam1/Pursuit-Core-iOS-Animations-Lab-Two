7f1c8a5c71b6fd0523849e837e7c0ed50c94cd98
Ahad Islam <ahad.islam624@gmail.com>
2020-02-20 16:14:15 -0500

    completed lab

--- animationslabtwo/animationslabtwo/ViewController.swift
+++ animationslabtwo/animationslabtwo/ViewController.swift
@@ -12,52 +12,121 @@ class ViewController: UIViewController {
     
     private lazy var linearButton: UIButton = {
         let button = UIButton()
+        button.tag = 0
         button.setTitle("Linear", for: .normal)
         return button
     }()
     
     private lazy var easeInButton: UIButton = {
         let button = UIButton()
+        button.tag = 1
         button.setTitle("Easein", for: .normal)
         return button
     }()
     
     private lazy var easeOutButton: UIButton = {
         let button = UIButton()
+        button.tag = 2
         button.setTitle("Easeout", for: .normal)
         return button
     }()
     
     private lazy var easeInEaseOutButton: UIButton = {
         let button = UIButton()
+        button.tag = 3
         button.setTitle("Easeineaseout", for: .normal)
         return button
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        return stackView
     }()
     
     private lazy var resetButton: UIButton = {
         let button = UIButton()
         button.setTitle("Reset", for: .normal)
+        button.addTarget(self, action: #selector(resetImages), for: .touchUpInside)
         return button
     }()
     
     private lazy var animateButton: UIButton = {
         let button = UIButton()
         button.setTitle("Animate", for: .normal)
+        button.addTarget(self, action: #selector(animateImages), for: .touchUpInside)
         return button
     }()
-
+    
+    private lazy var imageView: UIImageView = {
+        let iv = UIImageView()
+        return iv
+    }()
+    
+    private lazy var secondImageView: UIImageView = {
+        let iv = UIImageView()
+        return iv
+    }()
+    
+    private lazy var thirdImageView: UIImageView = {
+        let iv = UIImageView()
+        return iv
+    }()
+    
+    private lazy var fourthImageView: UIImageView = {
+        let iv = UIImageView()
+        return iv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupButtons()
         setupResetButton()
         setupAnimateButton()
+        setupImages()
+        print(view.frame.height / 2)
         view.backgroundColor = .systemBackground
-        // Do any additional setup after loading the view.
+    }
+    
+    @objc private func buttonPressed(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            imageView.isHidden.toggle()
+        case 1:
+            secondImageView.isHidden.toggle()
+        case 2:
+            thirdImageView.isHidden.toggle()
+        case 3:
+            fourthImageView.isHidden.toggle()
+        default:
+            break
+        }
+    }
+    
+    @objc private func animateImages() {
+        animateView(imageView, option: .curveLinear)
+        animateView(secondImageView, option: .curveEaseIn)
+        animateView(thirdImageView, option: .curveEaseOut)
+        animateView(fourthImageView, option: .curveEaseInOut)
+    }
+    
+    @objc private func resetImages() {
+        let images = [imageView, secondImageView, thirdImageView, fourthImageView]
+        images.forEach({$0.transform = CGAffineTransform(translationX: 0, y: 0)})
+    }
+    
+    
+    private func animateView(_ imageView: UIImageView, option: UIView.AnimationOptions) {
+        UIView.animate(withDuration: 3, delay: 0, options: [option], animations: {
+            imageView.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height / 2)
+        }, completion: nil)
     }
     
     private func setupButtons() {
         var buttons: [UIButton] = [linearButton, easeInButton, easeOutButton, easeInEaseOutButton]
-        let stackView = UIStackView(arrangedSubviews: buttons)
+        buttons.forEach({
+            stackView.addArrangedSubview($0)
+            $0.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
+        })
         buttons += [resetButton, animateButton]
         buttons.forEach({ $0.setTitleColor(.systemBlue, for: .normal)})
         stackView.axis = .horizontal
@@ -90,7 +159,29 @@ class ViewController: UIViewController {
             animateButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             animateButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12)])
     }
-
-
+    
+    private func setupImages() {
+        let images = [imageView, secondImageView, thirdImageView, fourthImageView]
+        images.forEach({
+            $0.contentMode = .scaleToFill
+            $0.image = UIImage(systemName: "rosette")
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.topAnchor.constraint(equalTo: stackView.bottomAnchor).isActive = true
+            $0.heightAnchor.constraint(equalToConstant: 120).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: view.frame.width/4).isActive = true
+        })
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            secondImageView.leadingAnchor.constraint(equalTo: imageView.trailingAnchor),
+            thirdImageView.leadingAnchor.constraint(equalTo: secondImageView.trailingAnchor),
+            fourthImageView.leadingAnchor.constraint(equalTo: thirdImageView.trailingAnchor)])
+    }
+    
+    
+    
+    
+    
 }
 
